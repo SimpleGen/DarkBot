50c9facf8c03e39b57a703dadc4604457607cf48
SimpleGen <45266498+SimpleGen@users.noreply.github.com>
2019-02-01 23:22:50 +0100

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -10,6 +10,9 @@ from discord import Game
 prefix = "-"
 bot = commands.Bot(command_prefix=prefix)
 
+chat_filter = ["CUNT", "BITCH", "FOTZE", "NIGGER", "JUDE", "SS", "HITLER", "ADOLF", "ADOLF HITLER", "NIGGA", "SLAVE", "SKLAVE", "JEWS", "KZ"]
+bypass_list = []
+
 @bot.event
 async def on_ready():
     print("ONLINE")
@@ -18,6 +21,18 @@ async def on_ready():
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name="Member")
     await bot.add_roles(member, role)
+    
+@client.event
+async def on_message(message):
+    contents = message.content.split(" ") #contents is a list type
+    for word in contents:
+        if word.upper() in chat_filter:
+            if not message.author.id in bypass_list:
+                try:
+                    await client.delete_message(message)
+                    await client.send_message(message.channel, "**YOU ARE CUTE**")
+                except discord.errors.NotFound:
+                    return 
     
 
 bot.run("NTQxMDE1NzA0OTg0NjE3MDEx.DzZUTA.pTnYdTKkRPxs78clH8s52W81y54")
